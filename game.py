aa3b45acfe8a20007bc8db4af9cab0caa84889b2
yu <tim233266@gmail.com>
2024-07-13 14:55:37 +0800

    1

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import random
 
 x = random.randint(1, 50)
+
 for i in range(5):
     y = eval(input("請猜一個數字:"))
     if x == y:
@@ -14,3 +15,5 @@ for i in range(5):
 
 if x != y:
     print(f"答案為:{x}")
+else:
+    print(f"共計猜{i+1}次")
