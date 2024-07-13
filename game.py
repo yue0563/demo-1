0db4d32f28049bea9fee29364d5fa7e84ace35d6
yu <tim233266@gmail.com>
2024-07-13 14:31:07 +0800

    新增猜大小功能

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
 import random
 
 x = random.randint(1, 50)
-print(x)
 for i in range(5):
     y = eval(input("請猜一個數字:"))
     if x == y:
         print("猜對了!")
         break
     else:
-        print("猜錯了~~")
+        if x > y:
+            print("猜高一點!")
+        else:
+            print("猜低一點!")
+
+if x != y:
+    print(f"答案為:{x}")
